78683785f0f72c3e3c573a90d1b9de5089242ac7
Gerardo López <gerardwjones@gmail.com>
2020-08-22 22:37:45 -0500

    Promises and asynchronism

--- src/es6/index.js
+++ src/es6/index.js
@@ -55,3 +55,52 @@ let team2 = ["jerry2","gerard2","shai2"]
 let education = ['David', ...team1, ...team2]
 
 console.log(education)
+
+// propiedad de objetos mejorada
+
+let name = 'Sherard'
+let age = 24
+
+let obj = {name: name, age: age} // before ES6
+
+let obj2 = {name, age} // after ES6
+
+console.log(obj, obj2)
+
+
+// Arrow Functions
+
+const names = [
+    { name: 'Shd', age: 24},
+    { name: 'Leslie', age: 18}]
+
+let listOfNames0 = names.map(function(item) { //Before ES6
+    console.log(item.name)
+})
+
+let listOfNames = names.map((x) => console.log(x.name)) //After ES6
+
+let listOfNames3 = (name, age, country) => {return 0}
+
+let listOfNames4 = name => 0
+
+const square = nume => num * num
+
+// Promesas y asincronismo
+
+const helloPromise = () =>{
+    return new Promise ((resolve, reject) => {
+        if(1){
+            resolve('Cool')
+        }
+        else {
+            reject('Nope')
+        }
+    })
+}
+
+helloPromise()
+.then(res => {console.log(res)})
+.then(() => console.log('Hello'))
+.then(() => console.log('Hello'))
+.catch(err => console.log(err))
